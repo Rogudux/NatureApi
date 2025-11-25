0a3f22fa7e0d09e83310092297bf506340c1edb7
Diego Sebastian Rojas Guerrero <diegorojas13579@gmail.com>
2025-11-25 16:33:05 -0600

    ffefgrgr

--- NatureAPi/Prompts.cs
+++ NatureAPi/Prompts.cs
@@ -5,7 +5,7 @@ public static class Prompts
     public static string GenerateTrailsPrompt(string jsonData)
     {
         return $@"
-        Eres un analista de datos experto en excursiones a lugares naturales de mexico.
+        Eres un analista de datos experto en excursiones y atracciones turisticas de lugares naturales en mexico.
         Analiza los sigueinte datos de trails (en JSON) {jsonData}
 
         Debes responder exclusivamente en formato JSON de la siguiente manera:
@@ -17,10 +17,10 @@ public static class Prompts
             ""patterns"": [string],
         }}
         En el apartado de patterns agrega apartados como: 
-        Cual es el trail mas turistico?
-        Cual es el trail mas cerca de ciudad de mexico?
+        Cual es el trail mas turistico de los disponibles?
+        Cual es el trail mas cercano de ciudad de mexico?
         Cual es el mas recomendado por los turistas extranjeros?
-        Si por alguna razon no puede generar esta respuesta valida, por ejeplo: te hacen falta datos o tienes algun error en el formato responde SOLO co el error, que falto.
+        Si por alguna razon no puede generar esta respuesta valida, por ejeplo: te hacen falta datos o tienes algun error en el formato responde cual fue el error, se claro con cual fue el error.
         No me saludes, no me des explicaciones, no me des comentarios y no incluyas texto adicional.
 ";
         
