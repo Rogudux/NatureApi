fdb405db2c66ab49b17cd1767af365c7f2df15b9
Diego Sebastian Rojas Guerrero <diegorojas13579@gmail.com>
2025-11-26 14:58:17 -0600

    Actualizacion del pormpt

--- NatureAPi/Prompts.cs
+++ NatureAPi/Prompts.cs
@@ -16,10 +16,10 @@ public static class Prompts
             ""avgEstimatedDistance"": string
             ""patterns"": [string],
         }}
-        En la parte de patterns responde las siguientes preguntas NO SOLO con la informacion del json apoyate de consultas a fuentes públicas (reseñas en Google/Tripadvisor, datos oficiales de visitas, y coordenadas geográficas) y del nombre de los trails:
-        Cual es el trail mas turistico de los disponibles?
-        Cual es el trail mas cercano de ciudad de mexico?
-        Cual es el mas recomendado por los turistas extranjeros?
+        En la parte de patterns responde las siguientes preguntas:
+        Cual es el trail mas dificil?
+        Cual es el trail mas largo en distancia?
+        Cual es el trail mas largo en tiempo?
         Si por alguna razon no puede generar esta respuesta valida, por ejeplo: te hacen falta datos o tienes algun error en el formato responde cual fue el error, se claro con cual fue el error.
         No me saludes, no me des explicaciones, no me des comentarios y no incluyas texto adicional.
 ";
